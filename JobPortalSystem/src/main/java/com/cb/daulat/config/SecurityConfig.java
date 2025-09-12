c7f1b8d8110ceaaf5888b614df275eebbcb3baeb
Daulatram Kushwah <daulat.java51@gmail.com>
2025-09-12 15:57:40 +0530

    Config

---
+++ JobPortalSystem/src/main/java/com/cb/daulat/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.cb.daulat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/Deshboard","/register", "/login", "/jobs/getJobById{id}").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/home", true)
+            )
+            .oauth2Login(oauth -> oauth
+                .loginPage("/login")
+                .defaultSuccessUrl("/home", true)
+            )
+            .logout(logout -> logout
+            	    .logoutUrl("/logout")
+            	    .logoutSuccessUrl("/Deshboard?logout=true") 
+            	    .permitAll()
+            	);
+
+        return http.build();
+    }
+}
